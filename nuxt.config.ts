d3008c6172386b51dc9cde2b75fb1bee33cd43b8
HasanovH93 <hasanovh14@gmail.com>
2025-01-31 15:39:53 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,10 +40,6 @@ export default defineNuxtConfig({
       link: [
         {
           rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-        },
-        {
-          rel: 'stylesheet',
           href: 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
         },
         {
@@ -53,15 +49,11 @@ export default defineNuxtConfig({
       ],
       script: [
         {
-          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
-        },
-        {
           src: 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
         },
         {
           src: 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js'
         },
-
       ]
     },
   },
